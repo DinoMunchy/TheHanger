8dc4cdafad826976a741c4f4e560e4bcd0f8faa5
DinoMunchy <Mattjmireles@yahoo.com>
2025-03-27 07:34:42 -0400

    Fix GitHub Pages routing and configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { CartProvider } from './context/CartContext';
 
 import Navbar from './components/Navbar';
@@ -13,19 +13,19 @@ import Cart from './pages/Cart';
 function App() {
   return (
     <CartProvider>
-      <Navbar />
-      <main className="min-h-screen bg-white">
-        <AnimatePresence mode="wait">
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <main className="pt-16">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/shop" element={<Shop />} />
-            <Route path="/shop/:id" element={<ProductDetails />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
-        </AnimatePresence>
-      </main>
+        </main>
+      </div>
     </CartProvider>
   );
 }
